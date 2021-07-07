3b28cc8d10162aefcc8ed5b4b78e2c5c148b0d13
Paweł Kołaczyński <kolaczyn@protonmail.com>
2021-07-07 12:27:05 +0200

    add 404 page

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Marta Portfolio
 
 ## TODO
-The scroll down button is clickable, even though it's invisible
+
+- The scroll down button is clickable, even though it's invisible
+- You have to scroll to see the footer in 404 page, also add nice bg to 404 page
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,30 @@
 import * as React from 'react';
+import { Helmet } from 'react-helmet';
+import ContainerSmall from '../components/container/ContainerSmall';
+import GeneralLayout from '../components/layout/GeneralLayout';
+import Button from '../components/ui/Button';
 
 const NotFoundPage = () => {
-  return <h1>404 not found</h1>;
+  return (
+    <>
+      <Helmet>
+        <title>Błąd 404 | Marta Krawczyk</title>
+      </Helmet>
+      <GeneralLayout>
+        <ContainerSmall className="min-h-screen font-sans flex items-center">
+          <div className="space-y-6">
+            <h1 className="font-sans font-bold">
+              <span className="text-black">Błąd 404,</span>
+              <br />
+              <span className="text-4xl">Żądana strona nie istnieje.</span>
+            </h1>
+
+            <Button primary>Powrót</Button>
+          </div>
+        </ContainerSmall>
+      </GeneralLayout>
+    </>
+  );
 };
 
 export default NotFoundPage;
